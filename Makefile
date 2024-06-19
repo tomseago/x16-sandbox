8f1d499a67c67c2b2c277bda8e0fac0422e64886
Tom Seago <tom@tomseago.com>
2024-06-18 21:41:33 -0700

    More hellop

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ ASSEMBLER6502	= acme
 AS_FLAGS	= -v8 --color -Wtype-mismatch --strict-segments
 RM		= rm
 
-PROGS		= acmehello.prg 
+PROGS		= acmehello.prg mtest.prg
 #PROGS		= acmehello.prg c64misc.prg ddrv128.prg ddrv64.prg macedit.prg reu-detect.prg trigono.o
 # Automatically generate a list of .prg files from the existing .asm files
 #PROGS = $(patsubst %.asm,%.prg,$(wildcard *.asm))
---
+++ hello.asm
@@ -0,0 +1,43 @@
+; Basic header
+  *= $0801
+  !word +
+  !word 10
+  !byte $9e
+  !text "2061"
+  !byte 0
++
+  !word 0
+
+  
+
+
+!address chrout = $ffd2
+
+printmsg:
+    ldy #$00
+loop:
+    lda .message,y
+    beq exit
+    jsr chrout
+    iny
+    bne loop
+
+exit:
+    rts
+
+.message
+    !pet "hello world!"
+    !byte 0
+
+
+
+
+;  !src "definitions.a"
+;  !src "rasterbar.a"
+; !src "hello.a"
+
+; Stay forever
+;- jmp -
+  rts
+
+;  !src "strings.a"
